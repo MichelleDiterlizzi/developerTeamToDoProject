6ccc2215b4d0052da0d7dabc8c41d43acb52da0d
Michelle Di Terlizzi <michelle.diterlizzi98@gmail.com>
2025-03-19 18:51:08 +0100

    Register view, authController y modelo user terminados

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -29,12 +29,52 @@ class AuthController extends Controller{
                 
                 header('Location: ' . $this->_baseUrl() . '/tasks');
                 exit;
-                
+
             } else {
                 $this->view->error = 'Email o password incorrectas';
             }
         }
     }
 
+    public function registerAction(){
+
+       
+        if (isset($_SESSION['user'])) {
+            header('Location: ' . $this->_baseUrl() . '/tasks');
+            exit;
+        }
+        
+        
+        if ($this->getRequest()->isPost()) {
+            $data = array(
+                'name' => $this->_getParam('user_name'),
+                'email' => $this->_getParam('email'),
+                'password' => password_hash($this->_getParam('password'), PASSWORD_DEFAULT),
+            );
+            
+            
+            if ($this->_userModel->findByEmail($data['email'])) {
+                $this->view->error = 'El correo ya está registrado';
+            } 
+            else if ($this->_getParam('password') !== $this->_getParam('confirm_password')){
+                $this->view->error = 'Las dos passwords no coinciden';
+            }
+            else if (empty($data['name']) || empty($data['email']) || empty($this->_getParam('password'))) {
+                $this->view->error = 'Todos los campos son obligatorios';
+            } else {
+                
+                $userId = $this->_userModel->save($data);
+                
+                header('Location: ' . $this->_baseUrl() . '/login');
+            }
+        }
+    }
+
+    public function logoutAction(){
+        session_destroy();
+        
+        header('Location: ' . $this->_baseUrl() . '/login');
+        exit;
+    }
 
 }
--- app/models/User.php
+++ app/models/User.php
@@ -9,13 +9,53 @@ class User {
     }
 
     protected function _loadData(){
-        
+
         $content = file_get_contents(ROOT_PATH . '/database/' . $this->_jsonFile);
-        $this->_data = json_decode($content,true);
+        $this->_data = json_decode($content);
         
         if (!is_array($this->_data)) {
             $this->_data = array();
         }
+    }
+
+    protected function _saveData(){
+        file_put_contents(
+            ROOT_PATH . '/database/' . $this->_jsonFile,
+            json_encode($this->_data, JSON_PRETTY_PRINT)
+        );
+    }
+
+    public function save($data = array()){
+        
+        $data = (object) $data;
+        
+        $data->id = uniqid();
+            
+        $this->_data[] = $data;
+            
+        $this->_saveData();
+        return $data->id;
+        
+    }
+
+    public function findByEmail($email){
+
+        foreach ($this->_data as $user) {
+            if ($user->email === $email) {
+                return $user;
+            }
+        }
+        
+    }
+
+    public function authenticate($email, $password)
+    {
+        $user = $this->findByEmail($email);
+        
+        if ($user && password_verify($password, $user->password)) {
+            return $user;
+        }
+        
     }
 
     
