d67fb9b0e6e2cb40a705703672a9614e78caed15
Michelle Di Terlizzi <michelle.diterlizzi98@gmail.com>
2025-03-19 17:40:56 +0100

    Front login y rutas

--- config/routes.php
+++ config/routes.php
@@ -1,14 +1,17 @@
-<?php 
+<?php
 
-/**
- * Used to define the routes in the system.
- * 
- * A route should be defined with a key matching the URL and an
- * controller#action-to-call method. E.g.:
- * 
- * '/' => 'index#index',
- * '/calendar' => 'calendar#index'
- */
 $routes = array(
-	'/test' => 'test#index'
+
+    '/login' => 'auth#login',
+    '/register' => 'auth#register',
+    '/logout' => 'auth#logout',
+    
+    '/task/show' => 'task#show',
+    '/tasks' => 'task#index', 
+    '/task/findTasks' => 'task#findTasks',
+    '/task/add' => 'task#add',
+    '/task/delete/:id' => 'task#delete',
+    '/task/move' => 'task#move', 
+
+    '/' => 'auth#login'
 );
